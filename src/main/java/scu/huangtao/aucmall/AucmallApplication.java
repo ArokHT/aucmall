d902d735d8693e559653eb53019d811f0f5b21a0
ArokHT <1037325587@qq.com>
2021-04-15 14:44:41 +0800

    commit test

---
+++ src/main/java/scu/huangtao/aucmall/AucmallApplication.java
@@ -0,0 +1,13 @@
+package scu.huangtao.aucmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AucmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scu/huangtao/aucmall/AucmallApplicationTests.java
@@ -0,0 +1,13 @@
+package scu.huangtao.aucmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
